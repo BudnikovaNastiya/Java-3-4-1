c4d32274cd364f5c51a489bfaa707205592409d3
Anastasiya Budnikova <budnikova.a.p@gmail.com>
2021-08-11 03:05:57 +0300

    initial commit afisha

---
+++ src/main/java/ru/netology/manager/AfishaManager.java
@@ -0,0 +1,57 @@
+package ru.netology.manager;
+
+import ru.netology.domain.Afisha;
+import lombok.Data;
+
+@Data
+
+public class AfishaManager {
+    int amountFilmsMax = 10;
+    private Afisha[] items = new Afisha[0];
+
+    public void add(Afisha item) {
+        // создаём новый массив размером на единицу больше
+        int length = items.length + 1;
+        Afisha[] tmp = new Afisha[length];
+
+
+        System.arraycopy(items, 0, tmp, 0, items.length);
+        // кладём последним наш элемент
+        int lastIndex = tmp.length - 1;
+        tmp[lastIndex] = item;
+        items = tmp;
+    }
+
+    public int setAmountFilmsMax(int newAmountFilmsMax) {
+        if (newAmountFilmsMax < 0) {
+            return amountFilmsMax;
+        }
+        amountFilmsMax = newAmountFilmsMax;
+        return amountFilmsMax;
+    }
+
+    public Afisha[] getAll() {
+        int resultLength;
+        if (items.length < amountFilmsMax) {
+            resultLength = items.length;
+        } else {
+            resultLength = amountFilmsMax;
+        }
+        // перебираем массив в прямом порядке
+        // но кладём в результаты в обратном
+        Afisha[] result = new Afisha[amountFilmsMax];
+        for (int i = 0; i < result.length; i++) {
+            int index = amountFilmsMax - 1 - i;
+            result[i] = items[index];
+        }
+        return result;
+    }
+
+    public AfishaManager() {
+    }
+
+    public AfishaManager(int amountFilmsMax) {
+        this.amountFilmsMax = amountFilmsMax;
+    }
+}
+
